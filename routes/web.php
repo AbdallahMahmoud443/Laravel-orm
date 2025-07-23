4d41c11e1eff6ab5a1187b1bcbc2d3213c67f032
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-23 17:53:05 +0300

    Save method

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,18 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {});
+Route::get('/', function () {
+    // title : save() method used to save data in database With (creating,updating) instance
+    // hint : create new Instance
+    /*
+    $user = new User();
+    $user->name = 'Abdullah Mahmoud';
+    $user->email = 'abdullah@gmail.com';
+    $user->password = '12345';
+    $user->save(); /
+    */
+    // hint : update instance
+    $user = User::find(1);
+    $user->name = 'magdy Ahmed';
+    $user->save();
+});

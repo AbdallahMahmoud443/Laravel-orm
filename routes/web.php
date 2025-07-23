9e26ae71a600f3d23feed0a51ce444cbb99e29b0
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 01:52:21 +0300

    get() method

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,13 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // hint: all() method returns all records from the database with all attributes at once
-    // $user = User::all();
-    //----------------------------------------//
-    // hint: all([attributes]) return all records from the database with only the specified attributes
-    //  $user = User::all(['name', 'email']); // return all users with name and email attributes
-    //----------------------------------------//
-    // hint: all([columnName as alias ]) return all records from the database with only the specified attributes with alias
-    $user = User::all(['name as username', 'email as user_email']);
-    dump($user);
-    dump($user[0]->username); // note: use alias instead of name
+    // title: all() vs get() methods in laravel orm
+    // hint: all() method returns a collection of all the records in the database, while get() method returns a collection of all the records that match the given conditions.
+    // hint: all() method is faster than get() method because it does not need to filter the records. However, get() method is more flexible because it allows you to pass conditions as parameters.
+    // hint: all() method is used when you want to retrieve all the records from the database, while get() method is used when you want to retrieve records that match certain conditions.
+    // Basic get() - no arguments
+    // $users = User::get();
+    // get() with specific columns
+    $users = User::where('is_admin', false)->get(['name', 'email']);
+    dd($users);
 });

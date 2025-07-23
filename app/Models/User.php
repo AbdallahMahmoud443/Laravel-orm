39fb54648c27fd3187c36831ba3177461f06c5db
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-23 18:44:50 +0300

    UpdateOrCreate vs updateOrInsert

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,12 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, SoftDeletes;
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $guarded = ['id'];
     /**
      * The attributes that should be hidden for serialization.
      *
--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,36 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // hint: insert() method add new instance in database return boolean value
-    // note: not insert created_at and updated_at fields Automatically
-    /* $is_inserting = User::insert([
+    // hint: updateOrCreate(condition,data) return Instance (Work With eloquent ORM)
+    /**
+     * Condition => True  Update Instance
+     * Condition => False Create Instance
+     */
+    // important: take care about non-nullable fields
+
+    // case 1 : Condition => false (create new Instance (John Doe Updated,john5@gmail.com))
+    /*  $user = User::updateOrCreate(['email' => 'john5@gmail.com'], [
+        'name' => 'John Doe Updated',
+        'password' => '12345678'
+    ]);*/
+
+    // case 2 : Condition => True (Update Instance)
+    /* $user = User::updateOrCreate(['email' => 'john5@gmail.com'], [
         'name' => 'John Doe',
-        'email' => 'john@gmail.com',
-        'password' => '12345',
-        'is_admin' => 0
     ]);*/
-    // hint: used to insert multi instance at once
-    /* $is_inserting = User::insert([
-        [
-            'name' => 'John Doe',
-            'email' => 'john2@gmail.com',
-            'password' => '12345',
-            'is_admin' => 0
-        ],
-        [
-            'name' => 'abdullah Ali',
-            'email' => 'abdullah@gmail.com',
-            'password' => '12345',
-            'is_admin' => 0
-        ]
-    ]);*/
-    // hint: insertGetId() method add new instance in database return ID value
-    $user_id = User::insertGetId([
-        'name' => 'abdullah Ali',
-        'email' => 'abdullah1@gmail.com',
-        'password' => '12345',
-        'is_admin' => 0
+    // hint: updateOrInsert(condition,data) return boolean  (Work With Query Builder and eloquent ORM)
+    // important prefer use updateOrInsert() with Query Builder, as same as updateOrCreate() In Usage
+    // note: return true with (update or insert), false when not do operating
+    // case 2 : Condition => True (Update Instance)
+    $user = DB::table('users')->updateOrInsert(['email' => 'john7@gmail.com'], [
+        'name' => 'John Doe',
+        'password' => '12345678' // with create should provide password because it isn't nullable field
+
     ]);
-    dd($user_id);
+    // case 1 : Condition => false (create new Instance (John Doe Updated,john7@gmail.com))
+    dump($user);
 });

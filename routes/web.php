3e7558c369977960096da795a8be7afc4c992715
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 04:39:51 +0300

    first() & firstOr() & firstOrFail()

--- routes/web.php
+++ routes/web.php
@@ -4,31 +4,14 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title : find(primary key or [keys],[attributes as alias]) & findOr(primary key ) & findOrFail(primary key)
-    // hint: find() & findOr() & findOrFail() are used to find a record from the database.
-
-    // hint: find() returns a single record or null if the record is not found.
-    // $user_1 = User::find(12);
-    // dump($user_1);
-    //--------------------------------------------------//
-    // $users = User::find([12, 13, 14]);
-    // $users = User::find([12, 13, 14], ['name as username']);
-    // dump($users);
-    //--------------------------------------------------//
-    // hint: findOr() returns a single record or creates a new record if the record is not found.
-    // $user_2 = User::findOr(
-    //     11,
-    //     function () {
-    //         return User::create([
-    //             'name' => 'Shazly',
-    //             'email' => 'Shazly3@example.com',
-    //             'password' => bcrypt('123456789')
-    //         ]);
-    //     }
-    // );
-    // dump($user_2);
-    //--------------------------------------------------//
-    // hint: findOrFail() returns a single record or throws a ModelNotFoundException if the record is not found.
-    // $user_3 = User::findOrFail(14);
-    // dump($user_3);
+    // title: first() & firstOr & firstOrFail()
+    // hint: first() will return the first record of the collection. If the collection is empty, it will return null.
+    // hint: firstOr() will return the first record of the collection or a default value if the collection is empty.
+    // hint: firstOrFail() will return the first record of the collection or throw an exception if the collection is empty.
+    $user_1 = User::where('is_admin', true)->first(['name', 'email']);
+    $user_2 = User::where('password', '1234561541254')->firstOr(function () {
+        return 'not found matched password'; // if the collection is empty, it will return this value
+    });
+    $user_3 = User::where('is_admin', 0)->firstOrFail(); // if the collection is empty, it will throw an exception
+    dump($user_1, $user_2, $user_3);
 });

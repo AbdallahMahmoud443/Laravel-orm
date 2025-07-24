094780911d38e97a68f14498e953a440b79c8d5f
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 18:57:24 +0300

    whereIn() & whereNotIn()

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,12 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title:  whereBetween() vs whereNotBetween() in laravel orm
-    // description:  whereBetween() and whereNotBetween() are two methods in Laravel's Eloquent ORM that allow you to filter records based on a range of values for a specific column.
-
-
-    // hint: whereBetween() method: The whereBetween() method allows you to filter records where the value of a specific column is between a given range of values. It takes two arguments: the name of the column and an array of two values representing the lower and upper bounds of the range.
-    // (withdraw>1000 and withdraw<5000) will be true
-    $users_1 = User::whereBetween('withdraw', [1000, 5000])->get();
-    // hint: whereNotBetween() method: The whereNotBetween() method allows you to filter records where the value of a specific column is not between a given range of values. It also takes two arguments: the name of the column and an array of two values representing the lower and upper bounds of the range.
-    // condition (withdraw>1000 and withdraw<5000) will be false
-    $users_2 = User::whereNotBetween('withdraw', [1000, 5000])->get();
-    dump($users_1, $users_2);
+    // title: whereIn(attribute, array) vs WhereNotIn(attribute, array)
+    // description: whereIn() and whereNotIn() are two methods in Laravel that are used to filter records based on a list of values.
+    // hint example: whereIn() is used to filter records where a column's value is in a list of values, while whereNotIn() is used to filter records where a column's value is not in a list of values.
+    // condition (id == 1 || id== 2 || id == 3) vs
+    $user_1 = User::whereIn('id', [1, 2, 3, 4])->get();
+    // condition (id != 1 && id != 2 && id != 3)
+    $user_2 = User::whereNotIn('id', [1, 2, 3])->get();
+    dump($user_1, $user_2);
 });

e2e7fdbb4d35fb3ba6b405532028870987362bc4
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-25 00:37:42 +0300

    reorder()

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title inRandomOrder()
-    // description: get records in random format from table when run every query (refresh page)
-    $users = User::inRandomOrder()->select('id', 'name', 'email')->get();
-    dump($users);
+    // title : reorder() method in laravel
+    // description : reorder() method in laravel is used to reorder the items in the collection based on the given key. It is used to sort the items in the collection based on the given key. The key can be a string or a closure that returns the value to be used for sorting. The method returns a new collection with the items reordered.
+    // usage if we need to rest order of items in collection,or make new order of items in collection
+    $users = User::orderBy('deposit', 'desc');
+    // note:next line used to reset previous order
+    // $query = $users->reorder('deposit'); // data return as collection ordered By Deposit Column
+    $query = $users->reorder('withdraw', 'desc'); // data return as collection ordered By Withdraw Column
+    dump($query->pluck('withdraw', 'name')->toArray());
 });

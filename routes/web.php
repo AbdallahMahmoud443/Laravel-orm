69fc81dc9204631afa04ac2a887e9700536558c5
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 18:30:37 +0300

    where()  & orWhere() in depth

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,43 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: min(attribute) & max(attribute) methods in laravel work with (collections)
-    // description: min() and max() methods in Laravel work with collections, not with raw queries. So, you can use these methods to get the minimum and maximum values of a column in a collection of models.
-    //hint min()
-    dump("min() method");
-    $min_withdraw = User::min('withdraw');
-    $min_withdraw_admins = User::where('is_admin', 1)->min('withdraw');
-    $min_deposit_users = User::where('is_admin', 0)->min('deposit');
-    // hint: avg(raw query)
-    $min_withdraw_and_deposit = User::min(DB::raw('deposit + withdraw'));
-    dump($min_withdraw, $min_withdraw_admins, $min_deposit_users, $min_withdraw_and_deposit);
-    dump("max() method");
-    // hint max()
-    $max_withdraw = User::max('withdraw');
-    $max_withdraw_admins = User::where('is_admin', 1)->max('withdraw');
-    $max_deposit_users = User::where('is_admin', 0)->max('deposit');
+    // title: where(attribute,operator,value or callback) method in laravel orm === && , orWhere(attribute,operator,value or callback) === ||  methods in laravel orm
+    // description: where() method is used to filter the records from the database. It is used to add a where clause to the query. It can be used to filter the records based on a single column or multiple columns.
 
-    $max_withdraw_and_deposit = User::max(DB::raw('deposit + withdraw'));
-    dump($max_withdraw, $max_withdraw_admins, $max_deposit_users, $max_withdraw_and_deposit);
+    // description: orWhere() method is used to add an or where clause to the query. It is used to add an additional condition to the query. It can be used to filter the records based on a single column or multiple columns.
+
+    $users_1 = User::where('id', 1)->get(); // where('id', 12) is equal to where('id', '=', 12)
+    $users_2 = User::where('deposit', '>', 1000)->get();
+    // where with multiple conditions (deposit > 1000 && id = 12)
+    $users_3 = User::where('deposit', '>', 1000)->where('id', 1)->get();
+    // where with multiple conditions using orWhere (deposit > 1000 || id = 12)
+    $users_4 = User::where('deposit', '>', 2000)->orWhere('id', 5)->get();
+    // where with multiple conditions using orWhere (deposit > 1000 && deposit < 2000)
+    $users_5 = User::where('deposit', '>', 1000)->where('deposit', '<', 2000)->get();
+    // second way for multiple conditions using Where (deposit > 1000 && deposit < 2000)
+    $users_6 = User::where([
+        ['deposit', '>', 1000],
+        ['deposit', '<', 2000],
+    ])->get();
+    // hint: orWhere (deposit > 1000 || is_admin = 0)
+    $users_7 = User::where('deposit', '>', 1000)->orWhere('is_admin', 0)->get();
+    // hint: orWhere (deposit > 1000 || withdraw > 1000 && is_admin = 0)
+    $users_8 = User::where('deposit', '>', 1000)->orWhere('withdraw', '>', 1000)->Where('is_admin', 0)->get();
+    // hint: where(callback) this approach work with filtering when you have multiple conditions
+    // $query is the instance of the query builder
+    $role = 1;
+    $users_9 = User::where(function ($query) use ($role) {
+        return $query->where('deposit', '>', 3000)->orWhere('withdraw', '>', 1000)->where('is_admin', $role);
+    })->get();
+    dump(
+        $users_1,
+        $users_2,
+        $users_3,
+        $users_4,
+        $users_5,
+        $users_6,
+        $users_7,
+        $users_8,
+        $users_9
+    );
 });

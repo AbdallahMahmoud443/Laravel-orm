14ddf3d8967c3f50fa6213984f92b82ea6d38b1a
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 18:53:28 +0300

    whereBetween() & whereNotBetween()

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,15 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title :  where() vs whereStrict() in laravel orm
-    // description: In Laravel, the where() and whereStrict() methods are used to add conditions to a query. The main difference between the two methods is that where() allows for loose comparisons (==), while whereStrict() allows for strict comparisons (===).
+    // title:  whereBetween() vs whereNotBetween() in laravel orm
+    // description:  whereBetween() and whereNotBetween() are two methods in Laravel's Eloquent ORM that allow you to filter records based on a range of values for a specific column.
 
-    $users = User::where('is_admin', 1)->get(); // return collection with element is_admin = 1
-    $users_1 = User::where('is_admin', 0)->get(); // return collection with element is_admin = 01
-    // important whereStrict() => work with collection only
-    dump($users_1->whereStrict('id', 1)); // return  collection with element id's = 1
-    dump($users_1->whereStrict('id', '1'));  // return  empty collection because '1' is not equal to 1
-    /**
-     * Use whereStrict() when dealing with:
-            1-Boolean values
-            2-Null checks
-            3-Type-sensitive data
-     */
+
+    // hint: whereBetween() method: The whereBetween() method allows you to filter records where the value of a specific column is between a given range of values. It takes two arguments: the name of the column and an array of two values representing the lower and upper bounds of the range.
+    // (withdraw>1000 and withdraw<5000) will be true
+    $users_1 = User::whereBetween('withdraw', [1000, 5000])->get();
+    // hint: whereNotBetween() method: The whereNotBetween() method allows you to filter records where the value of a specific column is not between a given range of values. It also takes two arguments: the name of the column and an array of two values representing the lower and upper bounds of the range.
+    // condition (withdraw>1000 and withdraw<5000) will be false
+    $users_2 = User::whereNotBetween('withdraw', [1000, 5000])->get();
+    dump($users_1, $users_2);
 });

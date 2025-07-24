8d0313ba6890b931ab7b1f4e7f70e9e69f529f0c
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 16:23:42 +0300

    count() method

--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,11 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title : value() method in Laravel
-    // hint: value() method is used to get the value of a (single) instance's variable
-    // hint: prefer work with value() with condition to return single instance of model
-    // pluck not work correctly with find() method
-    // $user = User::find()->pluck('name'); ❌
-    // $user = User::find(12)->value('name'); // return value of name column
-    // alternative way to get value of name column instead of $user->name
-    //important : if found more than one row match condition, it will return the first row
-
-    // $users_1 = User::where('is_admin', 1)->value('name'); // return value of name column of first matching row
-    // important : don't use first() method with value() method,it is not efficient
-    // $email = User::where('is_admin', '=', 1)->first()->value('email');❌
-    $email = User::where('is_admin', '=', 1)->value('email');
-    // hint: if you need user first and return only value do this
-    $email = User::where('is_admin', '=', 1)->first()?->email;
-    dump($email);
-
-    /**
-     * first() returns a full model instance, while value('email') extracts just the email string from that model. The second approach (->value('email') directly) is more efficient because it only retrieves the email column from the database instead of the entire record.
-     */
+    // title: count() method in  work with (collections)
+    // description: The count() method in Laravel is a convenient way to get the number of records in a database table. It returns the count of the records that match the given conditions, or all the records if no conditions are specified.
+    // example: Get the count of all users
+    $all_users_count = User::count();
+    $admins_count = User::where('is_admin', true)->count();
+    $users_count = User::where('is_admin', false)->count();
+    dump($all_users_count, $admins_count, $users_count);
 });

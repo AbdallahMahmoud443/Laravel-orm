0b618c740c037d43c250b000748ec62ea2e0e5b7
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 19:19:08 +0300

    firstWhere()

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,14 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: whereNull(attribute) vs WhereNotNull(attribute)
-    // description: whereNull() is used to filter records where a column's value is null, while whereNotNull() is used to filter records where a column's value is not null.
-
-    // condition (name == null)
-    $user_1 = User::whereNull('name')->get();
-    // condition (name != null)
-    $user_2 = User::whereNotNull('name')->get();
-    dump($user_1, $user_2);
+    // title: firstWhere(condition or conditions)
+    // description: get the first record that matches the condition.
+    // $user = User::where('is_admin', 1)->first(); // get the first record that matches the condition.
+    $user = User::firstWhere('is_admin', 1); // get the first record that matches the condition.
+    // with multiple conditions
+    $user_1 = User::firstWhere([
+        ['is_admin', 1],
+        ['deposit', '>', 2000]
+    ]);
+    dump($user, $user_1);
 });

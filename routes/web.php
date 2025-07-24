b2e8879c2dbf16d5bd5bed7984ba903fd3f7346c
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 17:11:56 +0300

    avg() method

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,13 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: sum($attribute) method in laravel work with (collections)
-    // description: sum($attribute) method in laravel work with (collections) and it return the sum of the attribute value in the collection
-    $total_withdraw = User::sum('withdraw');
-    $total_withdraw_admins = User::where('is_admin', 1)->sum('withdraw');
-    $total_deposit_users = User::where('is_admin', 0)->sum('deposit');
-    // hint: sum(raw query)
-    $total_withdraw_and_deposit = User::sum(DB::raw('deposit + withdraw'));
-    dump($total_withdraw, $total_withdraw_admins, $total_deposit_users, $total_withdraw_and_deposit);
+    // title: avg($attribute) method in laravel work with (collections)
+    // description: avg($attribute) method in laravel work with (collections) and not work with (raw query) return the average of the given column.
+
+    $avg_withdraw = User::avg('withdraw');
+    $avg_withdraw_admins = User::where('is_admin', 1)->avg('withdraw');
+    $avg_deposit_users = User::where('is_admin', 0)->avg('deposit');
+    // hint: avg(raw query)
+    $avg_withdraw_and_deposit = User::avg(DB::raw('deposit + withdraw'));
+    dump($avg_withdraw, $avg_withdraw_admins, $avg_deposit_users, $avg_withdraw_and_deposit);
 });

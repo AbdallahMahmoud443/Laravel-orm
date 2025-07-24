fd8002076ec92eb6405f4ad6444c677c456a2d35
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 21:10:15 +0300

    when() method

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,15 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: whereAny([attributes],operator,value) & whereAll();
-    // description: whereAny() will return true if any of the conditions are true, while whereAll() will return true only if all conditions are true.
-    // hint: whereAny() & whereAll() are used to filter the results of a query based on multiple conditions.
-    // hint: whereAny() common use to apply one condition on multiple columns.
-    $user = User::where('name', 'like', 'ahmed%')->orWhere('email', 'like', 'ahmed%')->get();
-    dump($user);
-    // Apply query with whereAny
-    // return all name or email start with ahmed
-    $user = User::whereAny(['name', 'email'], 'like', 'ahmed%')->get();
-    dump($user);
-    // hint: whereAll() common use to apply one condition on multiple columns.
-    $user = User::where('name', 'like', 'ahmed%')->Where('email', 'like', 'ahmed%')->get();
-    dump($user);
-    // Apply query with whereAny
-    // return all name and email start with ahmed
-    $user = User::whereAll(['name', 'email'], 'like', 'ahmed%')->get();
-    dump($user);
+    // title :  when(condition, callback=>true, callback=>false) method in laravel orm
+    // description : The when method allows you to conditionally run a query clause such as where, orWhere, having, or orWhereHaving. The callback provided to the when method will receive the query builder instance, allowing you to add additional constraints to the query.
+    // hint: when method is useful when you want to add a condition to a query based on a variable value. For example, you can use the when method to add a where clause to a query only if a certain condition is true.
+    // usage in filtering
+    $is_admin = 1;
+    $users = User::when($is_admin == 1, function ($q) use ($is_admin) {
+        return $q->where('is_admin', $is_admin);
+    }, function ($q) use ($is_admin) {
+        return $q->where('is_admin', $is_admin);
+    })->get();
+    dump($users);
 });

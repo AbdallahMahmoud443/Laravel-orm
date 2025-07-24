33e5d0ae032a36b78214794dd9d8c5f071c787bc
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 17:17:33 +0300

    min() & max()

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,22 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: avg($attribute) method in laravel work with (collections)
-    // description: avg($attribute) method in laravel work with (collections) and not work with (raw query) return the average of the given column.
-
-    $avg_withdraw = User::avg('withdraw');
-    $avg_withdraw_admins = User::where('is_admin', 1)->avg('withdraw');
-    $avg_deposit_users = User::where('is_admin', 0)->avg('deposit');
+    // title: min(attribute) & max(attribute) methods in laravel work with (collections)
+    // description: min() and max() methods in Laravel work with collections, not with raw queries. So, you can use these methods to get the minimum and maximum values of a column in a collection of models.
+    //hint min()
+    dump("min() method");
+    $min_withdraw = User::min('withdraw');
+    $min_withdraw_admins = User::where('is_admin', 1)->min('withdraw');
+    $min_deposit_users = User::where('is_admin', 0)->min('deposit');
     // hint: avg(raw query)
-    $avg_withdraw_and_deposit = User::avg(DB::raw('deposit + withdraw'));
-    dump($avg_withdraw, $avg_withdraw_admins, $avg_deposit_users, $avg_withdraw_and_deposit);
+    $min_withdraw_and_deposit = User::min(DB::raw('deposit + withdraw'));
+    dump($min_withdraw, $min_withdraw_admins, $min_deposit_users, $min_withdraw_and_deposit);
+    dump("max() method");
+    // hint max()
+    $max_withdraw = User::max('withdraw');
+    $max_withdraw_admins = User::where('is_admin', 1)->max('withdraw');
+    $max_deposit_users = User::where('is_admin', 0)->max('deposit');
+
+    $max_withdraw_and_deposit = User::max(DB::raw('deposit + withdraw'));
+    dump($max_withdraw, $max_withdraw_admins, $max_deposit_users, $max_withdraw_and_deposit);
 });

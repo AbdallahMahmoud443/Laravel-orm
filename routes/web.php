7902f3ae1736d448c9a2fdcdd8faa9bfa6eb9ca9
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 15:08:39 +0300

    pluck() method

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,14 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: first() & firstOr & firstOrFail()
-    // hint: first() will return the first record of the collection. If the collection is empty, it will return null.
-    // hint: firstOr() will return the first record of the collection or a default value if the collection is empty.
-    // hint: firstOrFail() will return the first record of the collection or throw an exception if the collection is empty.
-    $user_1 = User::where('is_admin', true)->first(['name', 'email']);
-    $user_2 = User::where('password', '1234561541254')->firstOr(function () {
-        return 'not found matched password'; // if the collection is empty, it will return this value
-    });
-    $user_3 = User::where('is_admin', 0)->firstOrFail(); // if the collection is empty, it will throw an exception
-    dump($user_1, $user_2, $user_3);
+    // title : pluck() method in Laravel
+    // hint: pluck(Attribute, Key) method is used to retrieve all values from a specific column in a database table.
+    // hint: pluck() method returns Collection of values. (base collection)
+    // hint: toArray() method is used to convert a collection to an array.
+    $emails = User::pluck('email')->toArray();
+    // note: return format (Ahmed => ahmed@gmail.com, Ali => ali@gmail.com)
+    $emails = User::pluck('email', 'name')->toArray();
+    //hint: pluck() with certain condition\
+    $emails = User::where('is_Admin', 1)->pluck('email')->toArray();
+    dump($emails);
 });

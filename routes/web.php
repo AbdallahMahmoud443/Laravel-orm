f1882890e63b5acc06633fc813d05b1924ee1a96
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-25 00:13:41 +0300

    orderBy() & orderByRaw()

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,20 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: whenEmpty(callback=>true,callback=>false) vs whenNotEmpty(callback=>true,callback=>false) in laravel
-    // description:  whenEmpty() and whenNotEmpty() are methods provided by the Laravel framework to conditionally display content based on the presence or absence of data in a collection or variable.
-    // important working with collection
-    $users = User::whereNull('name')->get(); // return data
-    $emails = User::whereNull('email')->get(); // return empty collection
-    // hint: whenEmpty()
-    $users->whenEmpty(function () {
-        dump('Collection is empty');
-    }, function () {
-        dump('Collection is not empty'); // print this
-    });
-    $emails->whenEmpty(function () {
-        dump('Collection is empty'); // print this
-    }, function () {
-        dump('Collection is not empty');
-    });
-
-    // hint: whenNotEmpty()
-    $users->whenNotEmpty(function () {
-        dump('Collection is not empty'); // print this
-    }, function () {
-        dump('Collection is empty');
-    });
-    $emails->whenNotEmpty(function () {
-        dump('Collection is Not empty');
-    }, function () {
-        dump('Collection is empty'); // print this
-    });
+    // title orderBy(attribute,mode) & orderByRaw(RawSQL) in laravel
+    // description: orderBy() & orderByRaw() in laravel are used to sort the data in the database.
+    // hint: orderBy() is used to sort the data in ascending order by default.
+    // hint: orderByRaw() is used to sort the data in ascending or descending order by using raw SQL query.
+    // example:
+    $users = User::orderBy('deposit', 'asc')->pluck('deposit', 'name')->toArray();
+    dump($users);
+    $users = User::orderByRaw('deposit desc')->pluck('deposit', 'name')->toArray();
+    dump($users);
+    // if i need arrange data based on the number of characters in the name
+    $users = User::orderByRaw('length(name) desc')->pluck('deposit', 'name')->toArray();
+    dump($users);
 });

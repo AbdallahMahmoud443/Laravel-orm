a7b48fc2d4ac6d7858db12f44dd70073046b4ab1
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-24 04:29:36 +0300

    find() & findOr() & findOrFail()

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,31 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title : select() & addSelect() methods
-    // hint: select($attributes) method is used to select specific columns from the table
-    // hint: addSelect($attributes) method is used to add additional columns to the selected columns
-    // hint: select('*') method is used to select all columns from the table
-    // hint: addSelect('*') method is used to add all columns to the selected columns
-    //  $users = User::select('name', 'email')->addSelect('created_at')->get();
-    $users = User::where('is_admin', true)->select('name as username', 'email as userEmail')->addSelect('created_at')->get();
-    // select($attributes) vs get($attributes)
-    // select($attributes) method returns a collection of objects with the specified attributes
-    // get($attributes) method returns a collection of objects with all attributes
-    // select($attributes) method is faster than get($attributes) method
-    // select($attributes) method is used to select specific columns from the table
-    dump($users);
+    // title : find(primary key or [keys],[attributes as alias]) & findOr(primary key ) & findOrFail(primary key)
+    // hint: find() & findOr() & findOrFail() are used to find a record from the database.
+
+    // hint: find() returns a single record or null if the record is not found.
+    // $user_1 = User::find(12);
+    // dump($user_1);
+    //--------------------------------------------------//
+    // $users = User::find([12, 13, 14]);
+    // $users = User::find([12, 13, 14], ['name as username']);
+    // dump($users);
+    //--------------------------------------------------//
+    // hint: findOr() returns a single record or creates a new record if the record is not found.
+    // $user_2 = User::findOr(
+    //     11,
+    //     function () {
+    //         return User::create([
+    //             'name' => 'Shazly',
+    //             'email' => 'Shazly3@example.com',
+    //             'password' => bcrypt('123456789')
+    //         ]);
+    //     }
+    // );
+    // dump($user_2);
+    //--------------------------------------------------//
+    // hint: findOrFail() returns a single record or throws a ModelNotFoundException if the record is not found.
+    // $user_3 = User::findOrFail(14);
+    // dump($user_3);
 });

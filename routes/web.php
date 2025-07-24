3858bb9b751aba6541e714a4b1c1a5fa7675f12a
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-25 00:25:15 +0300

    inRandomOrder()

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title latest(attribute) & oldest(attribute) in laravel
-    // description: latest(attribute) & oldest(attribute) in laravel are used to get the latest and oldest record from the database.
-    // hint: latest(attribute) is used to get the latest record from the database. (desc)
-    // hint: oldest(attribute) is used to get the oldest record from the database. (asc)
-    // title orderBy() & orderByRaw() in laravel (latest & oldest &orderByDesc & orderByRaw put) After any condition
-    // example:
-    $users = User::latest('deposit')->pluck('deposit', 'name')->toArray();
-    dump($users);
-    $users = User::oldest('deposit')->pluck('deposit', 'name')->toArray();
+    // title inRandomOrder()
+    // description: get records in random format from table when run every query (refresh page)
+    $users = User::inRandomOrder()->select('id', 'name', 'email')->get();
     dump($users);
 });

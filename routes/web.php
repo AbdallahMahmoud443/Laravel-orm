a13f9ae2990ae466884678604e728ae2771d58a4
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-25 21:15:41 +0300

    Advanced subqueries

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,27 @@
 <?php
 
+use App\Models\Post;
 use App\Models\User;
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title chunk() and chunkById()
-    // hint: chunk() is used to split the collection into smaller chunks of a specified size.
-    // hint: chunk() and chunkById() are used to split the collection into smaller chunks of a specified size. The difference between the two is that chunk() takes a size parameter, while chunkById() takes a size parameter and an optional ID parameter.
-    $users = User::all()->chunk(2); // return array of arrays each array has 2 users (work with collections)
-    // dump($users);
+    // title: subquery in laravel
+    // description: subquery is a query inside another query, it is used to fetch data from multiple tables and display it in a single query result.
+    // hint: to work with subquery in laravel orm ,should use addSelect() or select() methods.
+    // hint: use select() to identify the columns that will be fetched from the subquery.
+    // hint: use whereColumn() method to compare two columns in subquery.
+    // case: if I need to fetch all users and their last post , I can use subquery to fetch the last post  for each user and display it in the same query result.
+    // addSelect([column_name => query]) method is used to add a column to the query result.
+    // important: don't use get() method inside addSelect() method, because it will return the subquery result instead of the main query result.
+    // use orderByDesc() method to order the subquery result in descending order.
+    // use limit() method to limit the subquery result to the last row only.
 
-    // if i need to do something with each chunk,process data of users chunk by chunk
-    // useful when you have a large collection and you want to process it in smaller chunks to avoid memory issues or to improve performance.
-    $users = User::chunk(2, function ($users) {
-        /* foreach ($users as $user) {
-            // do something with each user
-        }*/
-        // dump($users);
-    });
-    //------------------------------------------------//
-    // hint: chunkById() is used to split the collection into smaller chunks of a specified size, but it also takes an optional ID parameter. it uses the ID parameter to determine the starting point of the chunk. This can be useful when you want to split the collection into chunks that are ordered by a specific column, such as an ID column. For example:
-    $users = User::orderBy('id')->chunkById(2, function ($users) {
-        dump($users); // useful for updating or deleting a large number of records in a database
-    }, 'id'); // id is the column name (Primary Key) that you want to use to split the collection into chunks
+    $users = User::addSelect(
+        [
+            'last_post_title' =>
+            Post::select('title')->whereColumn('user_id', 'users.id')->orderByDesc('id')->limit(1)
+        ]
+    )->get(); // return user with new column last_post_title contain the title of the last post for each user.
+    dump($users);
 });

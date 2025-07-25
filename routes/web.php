4fa5ff9bcbc64223391b7c5f82b66ef636f56a41
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-25 18:58:03 +0300

    whereHas() & whereDoesntHave()

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,17 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: has() & doesntHave() method in laravel Orm
-    // hint: has() method is used to filter the records that have a relationship with the given condition
-    // hint: doesntHave() method is used to filter the records that do not have a relationship with the given condition
-    // case : if I need to retrieve all users that have a posts, I can use the has() method like this:
-    $users = User::has('posts')->get();
+    // title: whereHas() & whereDoesntHave() method in laravel Orm
+    // hint: whereHas() is used to filter the records based on the relationship
+    // hint: whereDoesntHave() is used to filter the records which does not have the relationship
+    // case: if i need to get the users who have posts that likes greater than 450, i can use whereHas() method
+    $users = User::whereHas('posts', function ($q) {
+        return $q->where('likes', '>', 450);
+    })->get(); // this will return the users who have posts that likes greater than 450
     dump($users);
-    // case : if I need to retrieve all users that do not have a posts, I can use the doesntHave() method like this:
-    $users = User::doesntHave('posts')->get();
-    dump($users);
-    // case : if I need to retrieve all users that have a posts and the posts count is greater than 2, I can use the has() method like this:
-    $users = User::has('posts', '>=', 3)->get();
+    // case: if i need to get the users who do not have posts that likes greater than 450, i can use whereDoesntHave() method
+    $users = User::whereDoesntHave('posts', function ($q) {
+        return $q->where('likes', '>', 450);
+    })->get(); // this will return the users who do not have posts that likes greater than 450
     dump($users);
 });

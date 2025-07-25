f184d4d7f40094827c93f91a8d535b1a5547da07
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-25 19:37:26 +0300

    chunk() & chunkById()

--- routes/web.php
+++ routes/web.php
@@ -5,35 +5,23 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: with aggregates function in relationships
-    //description: Laravel provides a convenient way to access related data in your Eloquent models using the with method. This method allows you to specify the relationships that you want to preload, and it will automatically load the related data when you access it.
-    // hint: withCount() is used to count the number of related records for a given relationship. and return result as new column in parent model
-    // hint: withAvg() is used to calculate the average value of a given column in a related table. and return result as new column in parent model
-    // hint: withSum() is used to calculate the sum of a given column in a related table. and return result as new column in parent model
-    // hint: withMax() is used to get the maximum value of a given column in a related table. and return result as new column in parent model
-    // hint: withMin() is used to get the minimum value of a given column in a related table. and return result as new column in parent model
-    // hint: withExists() is used to check if a related record exists. and return result as new column in parent model
-    // hint: withCountDistinct() is used to count the number of distinct related records for a given relationship. and return result as new column in parent model
-    $user_with = User::withCount('posts')->get(); // withCount(relatedModel)
-    $user_with_avg = User::withAvg('posts', 'views')->get(); // withAvg(relatedModel, column)
-    $user_with_sum = User::withSum('posts', 'views')->get();  //withAvg(relatedModel, column)
-    $user_with_max = User::withMax('posts', 'views')->get();  //withAvg(relatedModel, column)
-    $user_with_min = User::withMin('posts', 'views')->get();  //withAvg(relatedModel, column)
-    $user_with_exists = User::withExists('posts')->get(); // withExists(relatedModel) return 0 => if user has no posts, 1 => if user has posts
+    // title chunk() and chunkById()
+    // hint: chunk() is used to split the collection into smaller chunks of a specified size.
+    // hint: chunk() and chunkById() are used to split the collection into smaller chunks of a specified size. The difference between the two is that chunk() takes a size parameter, while chunkById() takes a size parameter and an optional ID parameter.
+    $users = User::all()->chunk(2); // return array of arrays each array has 2 users (work with collections)
+    // dump($users);
 
-    // case if i need to get the number of distinct related records for a given relationship
-    $user_with_count_distinct = User::withCount(['posts' => function ($q) {
-        return $q->distinct('title'); // or whatever column you want to be distinct
-    }])->get(); // withCountDistinct(relatedModel)
-
-
-    dump(
-        $user_with,
-        $user_with_avg,
-        $user_with_sum,
-        $user_with_max,
-        $user_with_min,
-        $user_with_exists,
-        $user_with_count_distinct
-    );
+    // if i need to do something with each chunk,process data of users chunk by chunk
+    // useful when you have a large collection and you want to process it in smaller chunks to avoid memory issues or to improve performance.
+    $users = User::chunk(2, function ($users) {
+        /* foreach ($users as $user) {
+            // do something with each user
+        }*/
+        // dump($users);
+    });
+    //------------------------------------------------//
+    // hint: chunkById() is used to split the collection into smaller chunks of a specified size, but it also takes an optional ID parameter. it uses the ID parameter to determine the starting point of the chunk. This can be useful when you want to split the collection into chunks that are ordered by a specific column, such as an ID column. For example:
+    $users = User::orderBy('id')->chunkById(2, function ($users) {
+        dump($users); // useful for updating or deleting a large number of records in a database
+    }, 'id'); // id is the column name (Primary Key) that you want to use to split the collection into chunks
 });

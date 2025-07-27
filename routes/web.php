e0221f6d9a8f55fa9519bc125dc4f52459d1486e
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-27 16:19:05 +0300

    Muting events

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,19 @@ use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // last way to do model event is Create Observer for specific model
-    // run command php artisan make:observer UserObserver --model=User this command will create observer for User model (created,deleted,updated,....)
-    // this will trigger created method in observer when user created
-    User::create([
-        'name' => 'test',
-        'email' => 'test@gmail.com',
-        'password' => bcrypt('123456789')
-    ]);
-    // this will trigger Updated method in observer when user created
-    $user = User::find(13);
-    $user->update([
-        'name' => 'test 12',
-    ]);
-    // this will trigger deleted method in observer when user created
-    $user = User::find(13)->delete();
-    dump($user);
+    // title mutating events
+    // hint: withoutEvents() method is used to disable model events (first way)
+    /*  $user = User::withoutEvents(function () {
+        User::create([
+            'name' => 'test',
+            'email' => 'test123@gmail.com',
+            'password' => bcrypt('123456789')
+        ]);
+    });*/
+    // hint: using saveQuietly() is used to disable model events for saving,there are some methods like saveQuietly(),updateQuietly() and deleteQuietly() see documentation for more info. (second way)
+    $user = new User;
+    $user->name = 'test';
+    $user->email = 'test1234@gmail.com';
+    $user->password = bcrypt('123456789');
+    $user->saveQuietly();
 });

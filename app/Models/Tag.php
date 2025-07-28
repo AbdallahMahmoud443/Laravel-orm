136cac8bab83cb9ebffcf023452fd2472121554b
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-28 18:26:16 +0300

    Pivot table options

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -10,8 +10,9 @@ class Tag extends Model
 
     public function posts()
     {
-        // if you not care about naming conventions
-        //belongsToMany(Model,post_tag,post_id,tag_id)
-        return $this->belongsToMany(Post::class);
+        // pivot table options return relation with pivot table withPivot(columns)
+        // withTimestamps() return created_at and updated_at of pivot table
+        // as() is method to put custom name to pivot table
+        return $this->belongsToMany(Post::class)->as('CustomPostComments')->withPivot('tag_id', 'post_id')->withTimestamps();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,14 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title : many to many relationship
-    // $tags = Post::find(4)->tags; // return collection of tags belong to post
-    $posts = Tag::find(2)->posts; // return collection of posts belong to tag
-    //----------------------------------//
-    // $tags = Post::find(4)->tags()->pluck('name')->toArray();
-    // $posts = Tag::find(2)->posts()->orderBy('views', 'desc')->pluck('title')->toArray();
-    dump($posts);
+    // title : pivot table options (this wat tor retrieve columns in pivot table) use pivot
+    $tags = Tag::find(2);
+    /*
+    foreach ($tags->posts as $post) {
+        dump($post->pivot->created_at);
+    }*/
+    //using custom name of pivot table
+    foreach ($tags->posts as $post) {
+        dump($post->CustomPostComments->created_at);
+    }
 });

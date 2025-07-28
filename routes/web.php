1788e5252bfcc214a4d623ad95e67717ba2a1b41
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-28 15:11:46 +0300

    Retrieving relation rule

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,18 @@
 use App\Models\Post;
 use App\Models\Scopes\UserActiveScope;
 use App\Models\User;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // how to use relation one-to-one
+    // title:Retrieving relation rule
     /**
-     *
+     *  case of returned Relation
+     * instance->relation->property =>  is called Dynamic Property
+     * instance->relation()->anyChainOfModelOperations => is Query Builder Collection
      */
-    // relation one-to-one form user to phone
-    // $user = User::find(1);
-    // hint:this way return phone as  dynamic property (model instance)
-    // dump($user->phone->phone_number);
-    // hint:this way return phone() as  relation method (Query builder instance) and we can use query builder methods
-    // dump($user->phone());
-    // dump($user->phone()->pluck('phone_number', 'phone_type'));
-    // relation one-to-one form phone to user
-    $phone = \App\Models\Phone::find(1);
-    // dump($phone->user->name); // hint:this way return user as  dynamic property (model instance)
-    // dump($phone->user()->value('name')); // hint:this way return user() as  relation method (Query builder instance) and we can use query builder methods
+    // Example: return sum of (withdraw + deposit) of user has specific phone number it's id = 2
+    $phone = App\Models\Phone::find(2);
+    $sum_operations = $phone->user()->sum(DB::raw('withdraw + deposit'));
+    dump($sum_operations);
 });

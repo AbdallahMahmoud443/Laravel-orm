7b03c7394c9ae4661d1041cfc43759b09c99a09e
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-29 22:57:24 +0300

    save() & create() on relations

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,41 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: Eloquent Relationships Operations
-    // hint: every eloquent operations we do in single model,we can do it in Relations
-    // important: to do operation on relation,returned relation should be Query builder
-    // case if I need return most recent 2 posts from user id =2
-    // posts() is relation in form of query builder
-    $latest_posts = App\Models\User::find(2)->posts()->latest()->take(2)->get();
-    dump($latest_posts);
-    //case if I need return most views 2 posts from user id = 2
-    $most_views_posts = App\Models\User::find(2)->posts()->orderBy('views', 'desc')->take(2)->get();
-    dump($most_views_posts);
-    // case if I need sum of likes on post related to user id = 2
-    $sum_of_likes = App\Models\User::find(2)->posts()->sum('likes');
-    dump($sum_of_likes);
-    // case if I need posts it's likes greater than 300 for user id = 2
-    $posts_with_likes_greater_than_200 = App\Models\User::find(2)->posts()->where('likes', '>', 300)->get();
-    dump($posts_with_likes_greater_than_200);
+    // title: Eloquent Relationships Operations (save() & create() on relationships)
+    // case if I need create post for user's id = 3
+    $user = App\Models\User::find(3);
+    // this line will create post for user's id = 3 (create() method)
+    /*  $post = $user->posts()->create([
+        'title' => 'Learn javascript',
+        'likes' => 0,
+        'views' => 0,
+    ]);*/
+    //------------------------------------------------//
+    // second way to create instance related to another instance (save() method)
+    /* $post = new App\Models\Post();
+    $post->title = 'Learn Python';
+    $post->likes = 0;
+    $post->views = 0;
+    $user->posts()->save($post); // this line of code will create post for user's id = 3 (save() method)*/
+    //-------------------------------------------------------------//
+    // create multiple posts for user's id = 3 using createMany(arrayOfArrays) method
+    /* $user->posts()->createMany([
+        [
+            'title' => 'Learn PHP',
+            'likes' => 0,
+            'views' => 0,
+        ],
+        [
+            'title' => 'Learn Laravel',
+            'likes' => 0,
+            'views' => 0,
+        ],
+    ]);*/
+    //-------------------------------------------------------------//
+    // create multiple posts for user's id = 3 using saveMany(arrayOfInstances) method
+    $posts = [
+        new App\Models\Post(['title' => 'Learn wordpress', 'likes' => 0, 'views' => 0]),
+        new App\Models\Post(['title' => 'Learn LLM', 'likes' => 0, 'views' => 0]),
+    ];
+    $user->posts()->saveMany($posts);
 });

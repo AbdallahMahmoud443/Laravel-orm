4c25d8e8776c9b8d948e3d34bc0853cc6fb17046
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-30 02:27:46 +0300

    The same for polymorphic relationships

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,27 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Review;
 use App\Models\Tag;
+use App\Models\Video;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // title Relational Factories (Many to Many relationships)
-    // create one dummy post with 3 dummy  tags
-    // first way
-    // Post::factory()->has(Tag::factory()->count(3))->count(1)->create();
-    // second way
-    // create one dummy post with 2 dummy  tags with created_at = now
-    // hasAttached() is used to set the created_at attribute of the relationship in factory
-    // Post::factory()->hasAttached(Tag::factory()->count(2), ['created_at' => now()])->count(1)->create();
-    // third way
-    Post::factory()->hasTags(Tag::factory()->count(2))->count(1)->create();
+
     // important every thing applied on normal relation in applied in polymorphic relation (see documentation)
+    // MorphMany()
+    $post = Post::factory()->hasReview(3)->create();
+    // MorphTo()
+    $reviews = Review::factory()->count(3)->for(
+        Post::factory(), // must only one factory
+        'reviewable'
+    )->create();
+    // MorphToMany()
+    $videos = Video::factory()
+        ->hasAttached(
+            Tag::factory()->count(3),
+            ['public' => true]
+        )
+        ->create();
 });

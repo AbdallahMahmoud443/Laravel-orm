9b59527261af421b8ac15a50264b13a97cd02cda
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-30 02:20:00 +0300

    Many to Many relationships

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,19 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title Relational Factories
-    // create one Post with 1 user (Belongs to relationships )
-    // first Way
-    // create two posts for 1 new user
-    //Post::factory()->for(\App\Models\User::factory())->count(2)->create();
-    // second Way
-    // important : every thing do in single model factory we apple do here
-    // this way do when override attributes in user instances
-    /*$user = \App\Models\User::factory()->create();
-    Post::factory()->for($user)->count(1)->create();
-    Post::factory()->for($user)->count(1)->create([
-        'title' => 'Post title',
-    ]);*/
+    // title Relational Factories (Many to Many relationships)
+    // create one dummy post with 3 dummy  tags
+    // first way
+    // Post::factory()->has(Tag::factory()->count(3))->count(1)->create();
+    // second way
+    // create one dummy post with 2 dummy  tags with created_at = now
+    // hasAttached() is used to set the created_at attribute of the relationship in factory
+    // Post::factory()->hasAttached(Tag::factory()->count(2), ['created_at' => now()])->count(1)->create();
+    // third way
+    Post::factory()->hasTags(Tag::factory()->count(2))->count(1)->create();
+    // important every thing applied on normal relation in applied in polymorphic relation (see documentation)
 });

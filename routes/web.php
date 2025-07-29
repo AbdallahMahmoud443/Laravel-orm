f0b2bd39579fe2458082ec5aa7c33f81a8878575
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-30 02:08:07 +0300

    Belongs to relationships

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
-
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // title Relational Factories
-    // return one user with 3 posts (Has many relationships)
-    // first way
-    //  App\Models\User::factory()->has(App\Models\Post::factory()->count(3))->count(1)->create();
-    // second way (hasNameOfRelationship)
-    // hasPosts(number,[default attributes=>value])
-   //  App\Models\User::factory()->hasPosts(2, ['likes' => 2])->count(1)->create();
+    // create one Post with 1 user (Belongs to relationships )
+    // first Way
+    // create two posts for 1 new user
+    //Post::factory()->for(\App\Models\User::factory())->count(2)->create();
+    // second Way
+    // important : every thing do in single model factory we apple do here
+    // this way do when override attributes in user instances
+    /*$user = \App\Models\User::factory()->create();
+    Post::factory()->for($user)->count(1)->create();
+    Post::factory()->for($user)->count(1)->create([
+        'title' => 'Post title',
+    ]);*/
 });

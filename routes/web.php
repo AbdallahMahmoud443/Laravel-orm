85a048c75c29b45e5830e7585f5955f49cb46630
Abdallah Mahmoud <abdallahmahmoudnoby@gmail.com>
2025-07-30 01:54:55 +0300

    Data serialization

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // title: casting
-    $user = App\Models\User::find(2);
-    dump($user->deposit); // return deposit as string with $
+    // title: data serialization
+    // serialization in laravel is the process of converting an object into a array or json
+    $user = App\Models\User::find(2)->with('posts')->first();
+    dump($user->toArray()); // return relations with all attributes as array
+    dump($user->attributesToArray()); // doesn't return relations,but return all attributes as array
+    dump($user->toJson()); // return attributes with all attributes as json
 });
